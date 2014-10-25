09e4381541423aaf3a76494ed5ac1399cd333453
SkyWolf <hejun442001@126.com>
2014-10-25 11:01:42 +0800

    update Generater::numberToText() again.
    this mothed may batter then old one.
    
    this application is in early developing stage and not be functional.

--- generater.cpp
+++ generater.cpp
@@ -6,44 +6,47 @@ Generater::Generater()
 }
 
 
-
 QString Generater::numberToText(int n)
 {
-    QString ZhNum = "零一二三四五六七八九";
+    if (n == 0) return "零";
+    const QString numberName = "零一二三四五六七八九";
+    const QString unitName = "十百千";
+    const QString groupUnitName = "万亿";
     QString result;
-    if (n < 10) {
-        return QString(ZhNum[n]);
+    bool lessThenZero = false;
+    bool hideZeroFlag = false;
+    bool groupUnitFlag = false;
+    int bit = 0;
+
+    if (n < 0) {
+        lessThenZero = true;
+        n = -n;
     }
 
-    if (n > 99999999){
-        result = numberToText(n/100000000)+"亿";
-        n %= 100000000;
+    while (n) {
+        int x = n % 10;
+        n /= 10;
+        if ((bit % 4) == 0) groupUnitFlag = true;
+        if (groupUnitFlag) hideZeroFlag = true;
+
+        if (!hideZeroFlag) result = QString(numberName[x])
+                +((bit%4)>0 ? QString(unitName[(bit%4)-1]) : QString())
+                +(groupUnitFlag&&(bit/4)>0?QString(groupUnitName[(bit/4)-1]):QString())
+                +result;
+
+
+        hideZeroFlag = (x == 0);
+        groupUnitFlag = groupUnitFlag && hideZeroFlag;
+        ++bit;
     }
-    if (n > 9999)
-    {
-        result += numberToText(n/10000)+"万";
-        n %= 10000;
-    }
-    if (n > 999)
-    {
-        result += numberToText(n/1000)+"千";
-        n %= 1000;
-    }
-    if (n > 99)
-    {
-        result += numberToText(n/100)+"百";
-        n %= 100;
-    }
-    if (n > 9)
-    {
-        result += numberToText(n/10)+"十";
-        n %= 10;
-    }
-   return result;
+
+    if (lessThenZero) result = "负" + result;
+    return result;
 }
 
 QString Generater::generate(QString arg)
 {
+    // @TODO: Finish it
     QString result = numberToText(arg.toInt());
 
 
